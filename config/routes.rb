f54a5a45096ff150c67326a0e372f8239d6b1d40
Brad Smith <brad@smithwebtek.com>
2019-11-04 11:50:25 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :resources, only: [:index, :new, :create, :destroy]
+	root to: 'resources#index'
+
+	resources :resources, only: [:index, :show, :new, :create]
+
+	get 'cloudinary_index', to: 'resources#cloudinary_index'
+
+	get 'pdfs', to: 'resources#pdfs'
+	get 'images', to: 'resources#images'
+	get 'new', to: 'resources#new'
+	post '/new_upload', to: 'resources#new_upload'
+
+	get 'load_resources', to: 'resources#load_resources'
+
+	get '/destroy', to: 'resources#destroy'
+	get '/confirm_destroy', to: 'resources#confirm_destroy'
 end
